bbca8a45331707e470b2c22168105ac16e46c71d
dao <1576539471@qq.com>
2016-07-30 12:58:26 +0800

    分支信息

--- app/src/main/java/com/d/dao/gituse/MainActivity.java
+++ app/src/main/java/com/d/dao/gituse/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ((TextView)findViewById(R.id.text)).setText("master");
+        ((TextView)findViewById(R.id.text)).setText("branch");
     }
 }
